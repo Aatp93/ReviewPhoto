706b7bd7176221567a01f3f920ee3a7bed594386
Antonio Tinoco Perez <tinocoperezantonio@gmail.com>
2023-01-16 10:03:58 +0100

    Tp_02

--- src/Controller/PhotoController.php
+++ src/Controller/PhotoController.php
@@ -7,23 +7,19 @@ use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PhotoRepository;
 
 class PhotoController extends AbstractController
 {
     #[Route('/', name: 'photo.list')]
-    public function list(): Response
+    public function list(PhotoRepository $photoRepository): Response
     {
-        return $this->render('list.html.twig', [
-            'controller_name' => 'PhotoController',
-        ]);
-
-        $photo = new Photo();
-        $photo-> setTitle('Ma première photo');
-        $photo -> setPostAt( new DateTimeImmutable());
-        $photos = [];
-        $photos[] = $photo;
-        return $this->render('photo/list.html.twig', ['photos' => $photos]) ;
+        // $photo = new Photo();
+        // $photo->setTitle('Ma première photo');
+        // $photo->setPostAt(new DateTimeImmutable());
+        // $photos = [];
+        // $photos[] = $photo;
+        $photos = $photoRepository->findAll();
+        return $this->render('photo/list.html.twig', ['photos' => $photos]);
     }
-
-
 }
---
+++ src/DataFixtures/PhotoFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+use App\Entity\Photo;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class PhotoFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+            for ($i = 1; $i <=5; $i++){
+                $photo = new Photo();
+                $photo -> setTitle('Photo numéro' .$i);
+                $photo -> setPostAt((new \DateTimeImmutable()) -> add(\DateTimeImmutable:: createFromDateString('-'.$i.'week')));
+                $manager -> persist($photo); 
+
+            }
+            
+
+        $manager->flush();
+    }
+}
